985b26955cc54c64a8aaedd1221295eca99ddf7d
DuyPhuong <phuong.dinh@yourgolftravel.com>
2022-09-08 23:27:47 +0700

    Update table and create API CRUD test

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -4,15 +4,13 @@ module.exports = {
 
 
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('users', [{
+    return queryInterface.bulkInsert('Users', [{
       email: 'admin@gmail.com',
       password: '123456',
       firstName: 'Nguyen',
       lastName: 'Rin',
       address: 'HCM',
       gender: 1,
-      typeRole: 'ROLE',
-      keyRole: 'R1',
 
       createdAt: new Date(),
       updatedAt: new Date()
